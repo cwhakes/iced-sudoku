69693e801da60599e543c4d37db5d1fc263e893e
Will Hakes <info@cwilliamhakes.com>
2020-04-20 22:22:27 -0600

    Implement text input

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 mod sudoku;
 
-use sudoku::Sudoku;
+use sudoku::{COLUMNS, Cell, ROWS, Sudoku};
 
-use iced::{Column, Element, Row, Sandbox, Text};
+use iced::{Column, Container, Element, Length, Row, Sandbox, Text};
+use iced::text_input::{State, TextInput};
 
-#[derive(Default)]
 struct SudokuView {
     sudoku: Sudoku,
+    states: Vec<State>,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Debug)]
 pub enum Message {
-    Placeholder,
+    ChangedCell{
+        new_value: String,
+        cell_index: usize,
+    }
+
 }
 
 impl Sandbox for SudokuView {
@@ -19,7 +24,8 @@ impl Sandbox for SudokuView {
 
     fn new() -> SudokuView {
         SudokuView {
-            sudoku: Sudoku::default(),
+            sudoku: Sudoku::new(),
+            states: vec![State::new(); ROWS * COLUMNS],
         }
     }
 
@@ -30,12 +36,12 @@ impl Sandbox for SudokuView {
     fn view(&mut self) -> Element<Message> {
         //Text::new(self.text).size(50).into()
         let mut grid = Column::new();
-        let mut iter = self.sudoku.iter();
+        let mut iter = self.sudoku.iter().zip(self.states.iter_mut()).enumerate();
         for _ in 0..sudoku::ROWS {
             let mut row = Row::new();
-            for _ in 0..sudoku::COLUMS {
-                let num = iter.next().unwrap().read();
-                row = row.push(Text::new(num.to_string()));
+            for _ in 0..sudoku::COLUMNS {
+                let (index, (cell, state)) = iter.next().unwrap();
+                row = row.push(element_from_cell(index, cell, state));
             }
             grid = grid.push(row);
         };
@@ -44,9 +50,41 @@ impl Sandbox for SudokuView {
 
     fn update(&mut self, message: Message) {
         match message {
+            Message::ChangedCell{new_value, cell_index} => {
+                if new_value == "" {
+                    self.sudoku.0[cell_index / ROWS][cell_index % ROWS].set(0)
+                } else if let Ok(val) = new_value.parse() {
+                    self.sudoku.0[cell_index / ROWS][cell_index % ROWS].set(val);
+                }
+            }
             _ => {}
         }
     }
+}
+
+fn element_from_cell<'a>(index: usize, cell: &'a Cell, state: &'a mut State) -> Element<'a, Message> {
+    let inner: Element<_> = match cell {
+        Cell::Fixed(inner) => Text::new(inner.to_string()).into(),
+        Cell::Variable(_) => {
+            let value = match cell.read() {
+                0 => "".to_string(),
+                num @ _ => num.to_string(),
+            };
+            TextInput::new(
+                state,
+                "",
+                &value,
+                move |val| Message::ChangedCell {
+                    new_value: val.to_owned(),
+                    cell_index: index,
+                },
+            ).into()
+        }
+    };
+    Container::new(inner)
+        .width(Length::Units(32))
+        .height(Length::Units(32))
+        .into()
 }
 
 fn main() {
--- src/sudoku.rs
+++ src/sudoku.rs
@@ -1,10 +1,12 @@
 use std::sync::atomic::{AtomicU8, Ordering};
 
-pub const COLUMS: usize = 9;
+use rand::{thread_rng, Rng};
+
+pub const COLUMNS: usize = 9;
 pub const ROWS: usize = 9;
 
 #[derive(Clone, Debug, Default)]
-pub struct Sudoku(pub [[Cell; COLUMS]; ROWS]);
+pub struct Sudoku(pub [[Cell; COLUMNS]; ROWS]);
 
 #[derive(Debug)]
 pub enum Cell {
@@ -17,6 +19,21 @@ pub struct SudokuIter<'a, T: 'a + Iterator<Item=&'a Cell>> {
 }
 
 impl Sudoku {
+    pub fn new() -> Sudoku {
+        let mut sudoku = Sudoku::default();
+        for row in sudoku.0.iter_mut() {
+            for cell in row.iter_mut() {
+                let num = thread_rng().gen_range(1, 10);
+                if thread_rng().gen() {
+                    *cell = Cell::Fixed(num);
+                } else {
+                    *cell = Cell::Variable(AtomicU8::new(0));
+                }
+            }
+        }
+        sudoku
+    }
+
     pub fn iter(&self) -> impl '_ + Iterator<Item=&'_ Cell> {
         SudokuIter {
             inner: self.0.iter().flat_map(|i| i.into_iter())
@@ -29,6 +46,12 @@ impl Cell {
         match self {
             Cell::Fixed(inner) => *inner,
             Cell::Variable(inner) => inner.load(Ordering::Relaxed)
+        }
+    }
+
+    pub fn set(&mut self, new_value: u8) {
+        if let Cell::Variable(inner) = self {
+            inner.store(new_value, Ordering::Relaxed)
         }
     }
 }
