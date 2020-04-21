d809e940dd00af88472966d0b6fb86e289ee5192
Will Hakes <info@cwilliamhakes.com>
2020-04-21 01:41:15 -0600

    Add a sudoku solver

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod sudoku;
 
-use sudoku::{COLUMNS, Cell, ROWS, Sudoku};
+use sudoku::{Cell, Sudoku};
 
 use iced::{Column, Container, Element, Length, Row, Sandbox, Text};
 use iced::text_input::{State, TextInput};
@@ -11,21 +11,26 @@ struct SudokuView {
 }
 
 #[derive(Clone, Debug)]
+#[non_exhaustive]
 pub enum Message {
     ChangedCell{
         new_value: String,
         cell_index: usize,
     }
-
 }
 
 impl Sandbox for SudokuView {
     type Message = Message;
 
     fn new() -> SudokuView {
+        let mut sudoku = Sudoku::default();
+        //dbg!(sudoku.row(5).collect::<Vec<_>>());
+        //dbg!(sudoku.column(5).collect::<Vec<_>>());
+        sudoku.solve().fix();
+        //dbg!(sudoku.subregion(5).collect::<Vec<_>>());
         SudokuView {
-            sudoku: Sudoku::new(),
-            states: vec![State::new(); ROWS * COLUMNS],
+            sudoku: sudoku,
+            states: vec![State::new(); sudoku::SUDOKU_AREA],
         }
     }
 
@@ -37,9 +42,9 @@ impl Sandbox for SudokuView {
         //Text::new(self.text).size(50).into()
         let mut grid = Column::new();
         let mut iter = self.sudoku.iter().zip(self.states.iter_mut()).enumerate();
-        for _ in 0..sudoku::ROWS {
+        for _ in 0..sudoku::SUDOKU_MAX {
             let mut row = Row::new();
-            for _ in 0..sudoku::COLUMNS {
+            for _ in 0..sudoku::SUDOKU_MAX {
                 let (index, (cell, state)) = iter.next().unwrap();
                 row = row.push(element_from_cell(index, cell, state));
             }
@@ -52,9 +57,10 @@ impl Sandbox for SudokuView {
         match message {
             Message::ChangedCell{new_value, cell_index} => {
                 if new_value == "" {
-                    self.sudoku.0[cell_index / ROWS][cell_index % ROWS].set(0)
-                } else if let Ok(val) = new_value.parse() {
-                    self.sudoku.0[cell_index / ROWS][cell_index % ROWS].set(val);
+                    self.sudoku[cell_index].set(0)
+                // Set the value if value successfully parses to u8 between 1 and 9
+                } else if let Ok(val @ 1..=sudoku::SUDOKU_MAX_U8) = new_value.parse() {
+                    self.sudoku[cell_index].set(val);
                 }
             }
             _ => {}
@@ -68,6 +74,7 @@ fn element_from_cell<'a>(index: usize, cell: &'a Cell, state: &'a mut State) -> 
         Cell::Variable(_) => {
             let value = match cell.read() {
                 0 => "".to_string(),
+                // We
                 num @ _ => num.to_string(),
             };
             TextInput::new(
--- src/sudoku.rs
+++ src/sudoku.rs
@@ -1,12 +1,16 @@
+use std::ops;
 use std::sync::atomic::{AtomicU8, Ordering};
 
 use rand::{thread_rng, Rng};
 
-pub const COLUMNS: usize = 9;
-pub const ROWS: usize = 9;
+pub const SUBREGION_COLUMNS: usize = 3;
+pub const SUBREGION_ROWS: usize = 3;
+pub const SUDOKU_MAX: usize = SUBREGION_COLUMNS * SUBREGION_ROWS;
+pub const SUDOKU_MAX_U8: u8 = SUDOKU_MAX as u8;
+pub const SUDOKU_AREA: usize = SUDOKU_MAX * SUDOKU_MAX;
 
 #[derive(Clone, Debug, Default)]
-pub struct Sudoku(pub [[Cell; COLUMNS]; ROWS]);
+pub struct Sudoku(pub [[Cell; SUDOKU_MAX]; SUDOKU_MAX]);
 
 #[derive(Debug)]
 pub enum Cell {
@@ -14,8 +18,12 @@ pub enum Cell {
     Variable(AtomicU8),
 }
 
-pub struct SudokuIter<'a, T: 'a + Iterator<Item=&'a Cell>> {
-    inner: T,
+pub fn is_valid_subregion<'a>(iter: impl Iterator<Item=&'a Cell>) -> bool {
+    let mut vec: Vec<_> = iter.map(|c| c.read()).filter(|i| *i != 0).collect();
+    vec.sort();
+    let len = vec.len();
+    vec.dedup();
+    len == vec.len()
 }
 
 impl Sudoku {
@@ -23,7 +31,7 @@ impl Sudoku {
         let mut sudoku = Sudoku::default();
         for row in sudoku.0.iter_mut() {
             for cell in row.iter_mut() {
-                let num = thread_rng().gen_range(1, 10);
+                let num = thread_rng().gen_range(0, SUDOKU_MAX_U8) + 1;
                 if thread_rng().gen() {
                     *cell = Cell::Fixed(num);
                 } else {
@@ -34,10 +42,126 @@ impl Sudoku {
         sudoku
     }
 
-    pub fn iter(&self) -> impl '_ + Iterator<Item=&'_ Cell> {
-        SudokuIter {
-            inner: self.0.iter().flat_map(|i| i.into_iter())
+    pub fn iter(&self) -> impl Iterator<Item=&'_ Cell> {
+        self.0.iter().flatten()
+    }
+
+    pub fn iter_mut(&mut self) -> impl Iterator<Item=&'_ mut Cell> {
+        self.0.iter_mut().flatten()
+    }
+
+    pub fn fixeds_iter(&self) ->  impl Iterator<Item=&'_ Cell> {
+        self.iter().filter(|cell| {
+            match cell {
+                Cell::Fixed(_) => true,
+                Cell::Variable(_) => false,
+            }
+        })
+    }
+
+    pub fn variables_iter(&self) -> impl Iterator<Item=&'_ Cell> {
+        self.iter().filter(|cell| {
+            match cell {
+                Cell::Fixed(_) => false,
+                Cell::Variable(_) => true,
+            }
+        })
+    }
+
+    pub fn row(&self, row: usize) -> impl Iterator<Item=&'_ Cell> {
+        assert!(row < SUDOKU_MAX);
+        self.0[row].iter()
+    }
+
+    pub fn column(&self, column: usize) -> impl Iterator<Item=&'_ Cell> {
+        assert!(column < SUDOKU_MAX);
+        self.0.iter().flat_map(move |row| row.iter().nth(column))
+    }
+
+    pub fn subregion(&self, subregion: usize) -> impl Iterator<Item=&'_ Cell> {
+        assert!(subregion < SUDOKU_MAX);
+        self.0.iter().skip(SUBREGION_ROWS * (subregion / SUBREGION_ROWS))
+            .take(SUBREGION_ROWS)
+            .flat_map(move |row| {
+                row.iter().skip(SUBREGION_COLUMNS * (subregion % SUBREGION_ROWS))
+                    .take(SUBREGION_COLUMNS)
+            })
+    }
+
+    pub fn validate_cell(&self, index: (usize, usize)) -> bool {
+        assert!(index.0 < SUDOKU_MAX && index.1 < SUDOKU_MAX);
+        is_valid_subregion(self.row(index.0)) &&
+        is_valid_subregion(self.column(index.1)) &&
+        is_valid_subregion(self.subregion(SUBREGION_ROWS * (index.0/SUBREGION_ROWS) + index.1 % SUBREGION_COLUMNS))
+    }
+    
+    pub fn has_unique_solution(&self) -> bool {
+        let mut sudoku = self.clone();
+        // Set all variable cells to 0
+        sudoku.iter_mut().for_each(|cell| cell.set(0));
+        let stack = sudoku.make_solve_stack();
+        // If sudoku is complete, there is only 1 solution
+        if 0 == dbg!(stack.len()) {return true;}
+
+        let cursor = sudoku.solve_inner(&stack, 0);
+        // If this can't solve, there are no unique solutions
+        if 0 == cursor {return false;}
+
+        // back off from the end
+        let cursor = cursor - 1;
+        // Go beyond the previously found solution
+        // If the number is too big, we set to 0 when we solve inner
+        stack[cursor].1.fetch_add(1, Ordering::Relaxed);
+        sudoku.solve_inner(&stack, cursor);
+
+        // If true, there are no addtional solutions
+        0 == stack[0].1.load(Ordering::Relaxed)
+    }
+
+    pub fn solve(&mut self) -> &mut Self {
+        let stack = self.make_solve_stack();
+        self.solve_inner(&stack, 0);
+        self
+    }
+
+    fn make_solve_stack(&self) -> Vec<((usize, usize), &AtomicU8)> {
+        self.iter().enumerate().filter_map(|(index, cell)| {
+            if let Cell::Variable(inner) = cell {
+                Some(((index / SUDOKU_MAX, index % SUDOKU_MAX), inner))
+            } else {
+                None
+            }
+        }).collect()
+    }
+
+    fn solve_inner(&self, stack: &[((usize, usize), &AtomicU8)], mut cursor: usize) -> usize {
+        while cursor < stack.len() {
+            let (index, cell) = stack[cursor];
+            if SUDOKU_MAX_U8 > cell.fetch_add(1, Ordering::Relaxed) {
+                if self.validate_cell(index) {
+                    cursor += 1;
+                }
+            } else {
+                cell.store(0, Ordering::Relaxed);
+                if cursor > 0 {
+                    cursor -= 1;
+                } else {
+                    break;
+                }
+            }
         }
+        cursor
+    }
+
+    pub fn fix(&mut self) -> &mut Self {
+        for cell in self.iter_mut() {
+            let num = cell.read();
+            if num != 0 {
+                *cell = Cell::Fixed(num);
+            }
+        }
+
+        self
     }
 }
 
@@ -56,6 +180,38 @@ impl Cell {
     }
 }
 
+impl ops::Index<usize> for Sudoku {
+    type Output = Cell;
+
+    fn index(&self, idx: usize) -> &Self::Output {
+        assert!(idx < SUDOKU_MAX * SUDOKU_MAX);
+        &self.0[idx / SUDOKU_MAX][idx % SUDOKU_MAX]
+    }
+}
+
+impl ops::IndexMut<usize> for Sudoku {
+    fn index_mut(&mut self, idx: usize) -> &mut Self::Output {
+        assert!(idx < SUDOKU_AREA);
+        &mut self.0[idx / SUDOKU_MAX][idx % SUDOKU_MAX]
+    }
+}
+
+impl ops::Index<(usize, usize)> for Sudoku {
+    type Output = Cell;
+
+    fn index(&self, (row, column): (usize, usize)) -> &Self::Output {
+        assert!(row < SUDOKU_MAX && column < SUDOKU_MAX);
+        &self.0[row][column]
+    }
+}
+
+impl ops::IndexMut<(usize, usize)> for Sudoku {
+    fn index_mut(&mut self, (row, column): (usize, usize)) -> &mut Self::Output {
+        assert!(row < SUDOKU_MAX && column < SUDOKU_MAX);
+        &mut self.0[row][column]
+    }
+}
+
 impl Clone for Cell {
     fn clone(&self) -> Self {
         match self {
@@ -70,14 +226,20 @@ impl Clone for Cell {
 
 impl Default for Cell {
     fn default() -> Self {
-        Cell::Fixed(0)
+        Cell::Variable(AtomicU8::new(0))
     }
 }
 
-impl<'a, T: 'a + Iterator<Item=&'a Cell>> Iterator for SudokuIter<'a, T> {
-    type Item = &'a Cell;
+#[cfg(test)]
+mod test {
+    use super::*;
 
-    fn next(&mut self) -> Option<&'a Cell> {
-        self.inner.next()
+    #[test]
+    fn solver() {
+        let mut sudoku = Sudoku::default();
+        assert!(!sudoku.has_unique_solution());
+        sudoku.solve();
+        sudoku.fix();
+        assert!(sudoku.has_unique_solution());
     }
 }
